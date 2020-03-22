f361be7dea093fd10942d59905cd2bcabcdd9147
Rebecca Nghi <60793465+rnghigithub@users.noreply.github.com>
2020-03-22 19:53:43 -0400

    Add files via upload

--- wk6.js
+++ wk6.js
@@ -1,23 +1,24 @@
-const ul = document.getElementById("geo_app");
+const ul = document.getElementById('geo_app');
 const geoIpUrl = 'https://freegeoip.app/json';
 
-function getGeoIP(ip);{
+function getGeoIP(ip) {
     fetch(geoIpUrl)
-        .then( res => res.json ())
+        .then( res => res.json())
         .then(geoIp => {
               ip = geoIp; 
-
     console.log(ip.country_code);        
 
-const title = document.querySelector("#title");
+const body = document.querySelector('body');
+const contain2 = document.querySelector('.contain2');
+const Look = document.querySelector ('.title'); 
 title.innerHTML = 'Greetings! Click on the link below to locate where you are on Google Maps';
 
-const loaded = document.querySelector("#loaded");  
-const linked = document.queryCommandValue("#linked"); 
+const loaded = document.querySelector('#loaded');  
+const linked = document.queryCommandValue('#linked'); 
 linked.href = '';
 linked.textContent = '';
 
-const div = document.querySelector(".align");
+const div = document.querySelector('.align');
 div.innerHTML = 'Your <span> location </span> is coming from is <span class = "info"> ${ip[location_name]}</span>'; 
 
 function geoSuccess(position) {
@@ -51,10 +52,10 @@ function refresh() {
     document.location.reload();
 } 
 
-const btnRefresh = document.getElementById (".Look_For_This");
+const btnRefresh = document.getElementById ('.Look_For_This');
     btnRefresh.textContent = 'Refresh';
     btnRefresh.addEventListener ('click', refresh);
  
-const btnSuccess = document.querySelector (".Look");
+const btnSuccess = document.querySelector ('.Look');
     btnSuccess.textContent = 'Look';
     btnSuccess.addEventListener('click', getGeoIP);    
