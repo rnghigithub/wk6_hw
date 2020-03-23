d56b4f227e2a05508b8228ee842c0b1e3ed0c443
Rebecca Nghi <60793465+rnghigithub@users.noreply.github.com>
2020-03-22 20:42:15 -0400

    Add files via upload

--- wk6.js
+++ wk6.js
@@ -8,9 +8,12 @@ function getGeoIP(ip) {
               ip = geoIp; 
     console.log(ip.country_code);        
 
-const body = document.querySelector('body');
+const contain = document.querySelector('#contain');
 const contain2 = document.querySelector('.contain2');
-const Look = document.querySelector ('.title'); 
+
+//  cotain and contain 2 is a div class in html but not working, need to look more.  // 
+
+const title = document.querySelector('#title'); 
 title.innerHTML = 'Greetings! Click on the link below to locate where you are on Google Maps';
 
 const loaded = document.querySelector('#loaded');  
@@ -24,6 +27,9 @@ div.innerHTML = 'Your <span> location </span> is coming from is <span class = "i
 function geoSuccess(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
+
+// In the example shown the variables latitude and longitude work but not in my code // 
+
     status.textContent = '';
     linked.href = 'https://www.openstreetmap.org/#map=18/${latitude}/${longitude}'; 
     linked.textContent ='Coordinates are Latitude: ${latitude} and Longitude ${longitude}';
@@ -31,14 +37,14 @@ function geoSuccess(position) {
 
 function error() {
     loaded.textContent = 'Geolocation not supported on browser';
-    console.log ('status of ${loaded}');
+    console.log('status of ${loaded}');
 }
 
-if (!navigator.geolocation){
+if (!navigator.geolocation) {
     loaded.textContent = 'Geolocation not supported';
  } else {
     loaded.textContent = 'Loading Location...';
-    navigator.geolocation.getCurrentPosition (geoSuccess, error);
+    navigator.geolocation.getCurrentPosition(geoSuccess, error);
   }
 }) 
 
@@ -52,10 +58,10 @@ function refresh() {
     document.location.reload();
 } 
 
-const btnRefresh = document.getElementById ('.Look_For_This');
+const btnRefresh = document.getElementById('.Look_For_This');
     btnRefresh.textContent = 'Refresh';
-    btnRefresh.addEventListener ('click', refresh);
+    btnRefresh.addEventListener('click', refresh);
  
-const btnSuccess = document.querySelector ('.Look');
+const btnSuccess = document.querySelector('.Look');
     btnSuccess.textContent = 'Look';
     btnSuccess.addEventListener('click', getGeoIP);    
